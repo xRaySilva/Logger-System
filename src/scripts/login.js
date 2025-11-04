c126c4af5194bff5fea1896d7d4aae087d743961
Ray Silva <josenascimento0684@digitalcollege.internal>
2025-11-04 12:05:21 -0300

    first commit

---
+++ src/scripts/login.js
@@ -0,0 +1,32 @@
+const username = document.querySelector("#username-login")
+const password = document.querySelector("#password-login")
+const checkbox = document.querySelector("#checkbox-login")
+const buttonLogin = document.querySelector("#button-login")
+
+
+buttonLogin.addEventListener("click", (event) =>{
+
+    const usernameValue = username.value.toLowerCase()
+    const passwordValue = password.value.toLowerCase()
+
+
+    if(usernameValue === "" | passwordValue === ""){
+        event.preventDefault()
+        alert("Please fill in all fields")
+
+        return
+    }
+
+    if(usernameValue !== "alex" || passwordValue !== "123"){
+        event.preventDefault()
+        alert("Incorrect usarname or password!")
+
+        return
+    }
+
+    alert("Acess granted!")
+})
+
+checkbox.addEventListener("change", () => {
+    
+})
---
+++ src/scripts/start.js
@@ -0,0 +1,8 @@
+const buttonStart = document.querySelector("#start")
+
+
+buttonStart.addEventListener("click", () => {
+
+    window.location.href = "./src/pages/login.html";
+
+})

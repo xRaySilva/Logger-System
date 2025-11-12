50a3ae85d0ff3ab37993fae13a1d9935c173788a
Ray Silva <rayfortaleza1918@gmail.com>
2025-11-11 21:44:06 -0300

    atualizado com pagina de recuperacao de senha

---
+++ src/scripts/recovery.js
@@ -0,0 +1,43 @@
+const userName = document.querySelector("#userName-recovery")
+const newPassword = document.querySelector("#newPassword-recovery")
+const confirmPassword = document.querySelector("#confirm-recovery");
+
+const checkbox = document.querySelector("#checkbox-recovery")
+const buttonRecovery = document.querySelector("#button-recovery")
+
+
+buttonRecovery.addEventListener("click", (event) =>{
+    event.preventDefault()
+
+    const userNameValue = userName.value.toLowerCase()
+    const newPasswordValue = newPassword.value
+    const confirmPasswordValue = confirmPassword.value
+
+    if (!userNameValue || !newPasswordValue|| !confirmPasswordValue) {
+        
+      alert("Please fill in all fields");
+
+      return;
+    }
+
+    if(userNameValue !== sessionStorage.getItem("userName") || newPasswordValue !== confirmPasswordValue){
+
+        alert("Incorrect usarname or password!")
+
+        return
+    }
+
+    sessionStorage.setItem("password", newPasswordValue);
+
+    alert("Password Changed!")
+
+    window.location.href = "./login.html";
+
+})
+
+checkbox.addEventListener("change", () => {
+  const type = newPassword.getAttribute("type") === "password" ? "text" : "password";
+
+  newPassword.setAttribute("type", type);
+  confirmPassword.setAttribute("type", type);
+});

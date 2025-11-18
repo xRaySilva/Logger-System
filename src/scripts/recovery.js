e7bbf610dfc85e5c36a2ef1a214e6ef890159f11
Ray Silva <josenascimento0684@digitalcollege.internal>
2025-11-18 09:13:30 -0300

    projeto finalizado

--- src/scripts/recovery.js
+++ src/scripts/recovery.js
@@ -5,7 +5,6 @@ const confirmPassword = document.querySelector("#confirm-recovery");
 const checkbox = document.querySelector("#checkbox-recovery")
 const buttonRecovery = document.querySelector("#button-recovery")
 
-
 buttonRecovery.addEventListener("click", (event) =>{
     event.preventDefault()
 
@@ -16,14 +15,12 @@ buttonRecovery.addEventListener("click", (event) =>{
     if (!userNameValue || !newPasswordValue|| !confirmPasswordValue) {
         
       alert("Please fill in all fields");
-
       return;
     }
 
     if(userNameValue !== sessionStorage.getItem("userName") || newPasswordValue !== confirmPasswordValue){
 
         alert("Incorrect usarname or password!")
-
         return
     }
 
